b187ca4da928dce605fd8dad20c39fdf292b2742
Paulo Rezende <rezendepauloedu@outlook.com>
2018-10-01 11:22:29 -0300

    refactor: change babel-loader preset to match with Babel version 7

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['@babel/preset-env']
           }
         }
       },
